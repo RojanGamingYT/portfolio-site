7308ea4611da2e35c9e22c4d781920664b2d4af9
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2022-10-08 14:03:32 +0545

    Create arc-sw.js

---
+++ arc-sw.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting...</title>
+<script>
+    setTimeout(function(){
+        window.location.href = "https://arc.io/arc-sw.js";
+    }, 0);                
+</script>
+<link rel="canonical" href="https://arc.io/arc-sw.js">
+</html>
