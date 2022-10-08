14ab9f021c831b855f4c758f9eda834400384f92
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2022-10-08 14:29:50 +0545

    Update arc-sw.js

--- arc-sw.js
+++ arc-sw.js
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+ <meta http-equiv='Content-Type' content='Type=application/javascript; charset=utf-8'>
 <script>
     setTimeout(function(){
         window.location.href = "https://arc.io/arc-sw.js";
